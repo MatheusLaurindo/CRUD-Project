ef1ac3ecf3d735067d232d3b4efb293bf31bcbb0
Matheus <matheuslaurindo2017@gmail.com>
2022-07-22 23:36:01 -0300

    crud scaffolded e migration

---
+++ ProjetoCrud/Data/ProjetoCrudContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoCrud.Models;
+
+namespace ProjetoCrud.Data
+{
+    public class ProjetoCrudContext : DbContext
+    {
+        public ProjetoCrudContext (DbContextOptions<ProjetoCrudContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoCrud.Models.User> User { get; set; }
+    }
+}
--- ProjetoCrud/Startup.cs
+++ ProjetoCrud/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjetoCrud.Data;
 
 namespace ProjetoCrud
 {
@@ -33,6 +35,11 @@ namespace ProjetoCrud
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ProjetoCrudContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("ProjetoCrudContext"), builder =>
+            builder.MigrationsAssembly("ProjetoCrud")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
